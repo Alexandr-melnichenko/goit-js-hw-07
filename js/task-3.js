3d6920c95e479e719d2e8e413634c0a5e7a0cde6
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-08-24 15:02:39 +0300

    ready hw-7

--- js/task-3.js
+++ js/task-3.js
@@ -1,34 +1,15 @@
-class StringBuilder {
-#value = "";
-constructor (initialValue) {
-  this.#value = initialValue;
+const nameInput = document.querySelector("input#name-input");
+let nameOutput = document.querySelector("span#name-output");
+
+const handlInput = (event) => {
+    let userName = event.target.value.trim();
+    
+    if (userName !== " " && userName !== ""){
+        nameOutput.innerHTML = `${userName}`;
+    }
+    else {
+        nameOutput.innerHTML = "Anonymus";
+    }
 }
 
-getValue() {
-  return this.#value;
-}
-
-padEnd(str) {
-this.#value = this.#value + str;
-}
-
-padStart(str) {
-this.#value = str + this.#value;
-}
-
-padBoth(str) {
-this.#value = str + this.#value + str;
-}
-
-}
-
-
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener("input", handlInput);
